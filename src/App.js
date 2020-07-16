e7aeae1f19f978d86ef7433bbb8b52a3896b807f
azacor <azakor@gmail.com>
2020-07-16 18:25:53 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Auth from './componentes/Auth'
+import {useUser} from 'reactfire';
+
+
 
 function App() {
+  
+  const user = useUser();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5 justify-content-center">   
+{     user&&   <p>Usuario:{user.email}</p>}
+{     user&&   <p>BIENVENID@</p>}
+        <Auth/>
+
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export default
+{
+    apiKey: "AIzaSyCHaC9RENmPGH5u1KW-TkEUkx2Bz1kR-w8",
+    authDomain: "login-custocare.firebaseapp.com",
+    databaseURL: "https://login-custocare.firebaseio.com",
+    projectId: "login-custocare",
+    storageBucket: "login-custocare.appspot.com",
+    messagingSenderId: "640582114245",
+    appId: "1:640582114245:web:df467a822d4307bddf89ac"
+  };
